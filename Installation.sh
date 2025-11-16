00079a816fc94a0882c8fe5ef4ec569e163e3966
vijay <vijayanthony28667@gmail.com>
2025-11-16 22:49:19 +0530

    Updated

--- Installation.sh
+++ Installation.sh
@@ -3,9 +3,10 @@
 #install nginx , mysql , python3 in the server throgh the script
 #we will pass nginx , mysql python3 as the arguments to this script
 
-mkdir -p /var/log/shell_logs
-FILE_NAME=$( echo $0 | cut -d "." -f1 )
-
+LOGS_FOLDER="/var/log/shell_logs"
+mkdir -p $LOGS_FOLDER
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+FILE_NAME="$LOGS_FOLDER/$SCRIPT_NAME.log"
 USER_ID=$(id -u)
 if [ $USER_ID -ne 0 ]; then
     echo "Run with the root user please"
@@ -14,11 +15,11 @@ fi
 
 INSTALLING(){
     echo "Installing $1"
-    dnf install $1 -y &>> /var/log/shell_logs/$FILE_NAME.log
+    dnf install $1 -y &>> /var/log/shell_logs/$FILE_NAME
 }
 
 INSTALLED_CHECK(){
-    dnf list installed $1 -y &>> /var/log/shell_logs/$FILE_NAME.log
+    dnf list installed $1 -y &>> /var/log/shell_logs/$FILE_NAME
     if [ $? -ne 0 ]; then
         INSTALLING $1
     else 
