31463cfef9007e45acc1ebdb5136c0fd6dfd3d04
vijay <vijayanthony28667@gmail.com>
2025-12-09 12:31:16 +0530

    adding mogodb script

--- Shell-Roboshop/mongodb.sh
+++ Shell-Roboshop/mongodb.sh
@@ -1,2 +1,48 @@
 #!/bin/bash
 
+RED="\033[31m"
+GREEN="\033[1;32m"
+YELLOW="\033[4;33m"
+NORMAL="\033[0m"
+
+UIDD=$(echo $UID)
+if [ "$UIDD" -ne 0 ]; then
+    echo -e "Run with $RED ROOT USER $NORMAL"
+    exit 1
+fi
+
+FOLDER="/var/log/shell-logs"
+mkdir -p $FOLDER
+
+SCRIPT_NAME=$0
+FILE_NAME=$(cut -d "." -f1 ${SCRIPT_NAME})
+
+touch $FOLDER/$FILE_NAME.log
+
+VALIDATE(){
+    if [ $2 -eq 0 ]; then
+        echo -e "$1 .... $GREEN SUCCESS $NORMAL" | tee -a "$FOLDER/$FILE_NAME.log"
+
+    else
+        echo -e "$1 ... $RED FAILURE $NORMAL" | tee -a "$FOLDER/$FILE_NAME.log"
+        exit 1
+    fi
+}
+
+cp ./mongo.repo /etc/yum.repos.d/mongo.repo
+VALIDATE "Adding repo is" $?
+
+dnf install mongodb-org -y 
+VALIDATE "Installing Repo is" $?
+
+systemctl enable mongod 
+VALIDATE "Enabling Mongodb is" $?
+
+systemctl start mongod 
+VALIDATE "Starting Mongodb is" $?
+
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf
+VALIDATE "Exposed to all IP is" $?
+
+systemctl restart mongod
+VALIDATE "Restarted Mongodb is" $?
