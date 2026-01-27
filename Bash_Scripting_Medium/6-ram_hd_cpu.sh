6cfe6d5e0c541d944ad9a1e50afec58a43bb97f0
vijay <vijayanthony28667@gmail.com>
2026-01-27 09:40:11 +0530

    added the script for the disk,ram,cpu

--- Bash_Scripting_Medium/6-ram_hd_cpu.sh
+++ Bash_Scripting_Medium/6-ram_hd_cpu.sh
@@ -4,7 +4,7 @@ DISK_PERCENTAGE=$( df -hT | awk '{print $6f}'| grep -v Use% | cut -d "%" -f1
 )
 ALL=$(df -hT | awk '{print $7f,$6f}'| grep -v Mounted)
 
-for IFS= read -r line;
+while IFS= read -r line;
 do
     current=$(echo "$line" | awk '{print $2}' | cut -d "%" -f1)
     if [ "$current" -ge 2 ]; then
