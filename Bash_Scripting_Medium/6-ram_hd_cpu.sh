66ea0926c0ab419533aa5d5241c2181916b71058
vijay <vijayanthony28667@gmail.com>
2026-01-27 09:19:33 +0530

    added the script for the disk,ram,cpu

--- Bash_Scripting_Medium/6-ram_hd_cpu.sh
+++ Bash_Scripting_Medium/6-ram_hd_cpu.sh
@@ -6,7 +6,7 @@ ALL=$(df -hT | awk '{print $7f,$6f}'| grep -v Mounted)
 
 for IFS= read -r line;
 do
-    if [ $DISK_PERCENTAGE -ge 2 ]; then
+    if [ "$DISK_PERCENTAGE" -ge 2 ]; then
         echo "More usage on the $line"
     fi
-done <<< $ALL
+done <<< "$ALL"
